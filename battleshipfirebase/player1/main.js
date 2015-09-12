13ffcdff382c6e9459d1ec13a2aa840f1598e18a
Sanomi <snrutledge@gmail.com>
2015-09-12 00:26:22 -0700

    bug fix combined dom and database changes

--- battleshipfirebase/player1/main.js
+++ battleshipfirebase/player1/main.js
@@ -1,13 +1,14 @@
 'use strict';
 var ref1 = new Firebase('https://qnbattleship.firebaseio.com/player1');
+var ref2 = new Firebase('https://qnbattleship.firebaseio.com/player2');
+var refC = new Firebase('https://qnbattleship.firebaseio.com/counter');
 var player2Choices = [];
 var maxShip = 0;
+var counter = 0;
 $(document).ready(init);
 function init() {
-  var status = 'not ready';
 	ref1.remove();
 	$('button').click(function() {ref1.remove();});
-	$('#placeBoats td').click(player1Chooser);
   $('#placeBoats td').on('click', selectShips);
   ref1.on('child_added', player1CoOrdSend);
   console.log(player2Choices);
@@ -17,36 +18,40 @@ function player1CoOrdSend(snapshot) {
   var message = snapshot.val();
   console.log(message.co);
 }
-
-function player1Chooser() {
-	var co = $(this).attr('class') + $(this).parent().attr('class');
-    $(this).addClass('ship');
-	ref1.push( {co: co });    
-}
 function player2CoOrd() {
-  var ref2 = new Firebase('https://qnbattleship.firebaseio.com/player2');
   ref2.on('child_added', function(snapshot) {
     var message = snapshot.val();
     console.log(message.co);
     player2Choices.push(message.co);
     console.log(player2Choices);
-    if ( player2Choices.length === 15 ) {
+    if ( player2Choices.length === 2 ) {
     checkP2Match(player2Choices);
     }
 });
 };
   function selectShips(){
-  if (maxShip >=15) {
+  if (maxShip === 2) {
   alert("Out of Ships");
-  var status = 'ready'
-  return status;
+  $(this).off()
+  return;
   }
+    var co = $(this).attr('class') + $(this).parent().attr('class');
+    $(this).addClass('ship');
+    ref1.push( {co: co }); 
+
     maxShip++
     $(this).css('background-color', 'blue');
     $(this).off()
 }
 function checkP2Match(player2Choices) {
   $('#guesses td').click( function() {
+    counter++;
+    refC.remove();
+    refC.set({counter: counter});
+    refC.on('value', function(snapshot) {
+    var message = snapshot.val();
+    return message;
+  });
     var guessCoOrd = $(this).attr('class') + $(this).parent().attr('class');
     if ( player2Choices.indexOf(guessCoOrd) !== -1 ) {
       $(this).css('background-color', 'red')
