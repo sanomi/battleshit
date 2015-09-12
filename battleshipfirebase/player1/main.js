52c93cb9162564d2588aa0dac46981a6d8b2d072
Sanomi <snrutledge@gmail.com>
2015-09-12 02:58:15 -0700

    added turns

--- battleshipfirebase/player1/main.js
+++ battleshipfirebase/player1/main.js
@@ -1,13 +1,20 @@
 'use strict';
 var ref1 = new Firebase('https://qnbattleship.firebaseio.com/player1');
 var ref2 = new Firebase('https://qnbattleship.firebaseio.com/player2');
-var refC = new Firebase('https://qnbattleship.firebaseio.com/counter');
+var ref1G = new Firebase('https://qnbattleship.firebaseio.com/player1Guess');
+var ref2G = new Firebase('https://qnbattleship.firebaseio.com/player2Guess');
 var player2Choices = [];
 var maxShip = 0;
-var counter = 0;
+var myTurn = true;
 $(document).ready(init);
 function init() {
 	ref1.remove();
+  ref1G.remove();
+  ref2G.remove();
+  ref2G.on('child_added', function(guessCoOrd) {
+    console.log("It's your turn");
+    myTurn = true;
+  })
 	$('button').click(function() {ref1.remove();});
   $('#placeBoats td').on('click', selectShips);
   ref1.on('child_added', player1CoOrdSend);
@@ -24,13 +31,13 @@ function player2CoOrd() {
     console.log(message.co);
     player2Choices.push(message.co);
     console.log(player2Choices);
-    if ( player2Choices.length === 2 ) {
+    if ( player2Choices.length === 15 ) {
     checkP2Match(player2Choices);
     }
 });
 };
   function selectShips(){
-  if (maxShip === 2) {
+  if (maxShip === 15) {
   alert("Out of Ships");
   $(this).off()
   return;
@@ -44,19 +51,17 @@ function player2CoOrd() {
     $(this).off()
 }
 function checkP2Match(player2Choices) {
-  $('#guesses td').click( function() {
-    counter++;
-    refC.remove();
-    refC.set({counter: counter});
-    refC.on('value', function(snapshot) {
-    var message = snapshot.val();
-    return message;
-  });
-    var guessCoOrd = $(this).attr('class') + $(this).parent().attr('class');
-    if ( player2Choices.indexOf(guessCoOrd) !== -1 ) {
-      $(this).css('background-color', 'red')
-    } else {
-      $(this).css('background-color', 'green')
-    }
-  })
+
+    $('#guesses td').click( function() {
+      if (!myTurn) {return;}
+      var guessCoOrd = $(this).attr('class') + $(this).parent().attr('class');
+      if ( player2Choices.indexOf(guessCoOrd) !== -1 ) {
+        $(this).css('background-color', 'red');
+      } else {
+        $(this).css('background-color', 'green');
+      }
+      $(this).off();
+      myTurn = false;
+      ref1G.push( {p1: guessCoOrd} );
+    })
 }
--- battleshipfirebase/player2/main.js
+++ battleshipfirebase/player2/main.js
@@ -1,42 +1,74 @@
 'use strict';
-
-var ref = new Firebase('https://qnbattleship.firebaseio.com/');
+var ref1 = new Firebase('https://qnbattleship.firebaseio.com/player1');
+var ref2 = new Firebase('https://qnbattleship.firebaseio.com/player2');
+var ref1G = new Firebase('https://qnbattleship.firebaseio.com/player1Guess');
+var ref2G = new Firebase('https://qnbattleship.firebaseio.com/player2Guess');
+var player1Choices = [];
+var maxShip = 0;
+var counter = 0;
+var myTurn = false;
 
 $(document).ready(init);
 
 function init() {
-	ref.remove();
-	$('button').click(function() {ref.remove();});
-	$('td').click(coChooser);
-	ref.on("value", getData);
-	// ...
+    var status = 'not ready';
+    player1CoOrd();
+    ref2.remove();
+    ref1G.remove();
+    ref2G.remove();
+    ref1G.on('child_added', function(guessCoOrd) {
+    console.log("It's your turn");
+    myTurn = true;
+  })
+    
+    $('button').click(function() {ref2.remove();});
+    $('#placeBoats td').on('click', selectShips);
+  ref2.on('child_added', player2CoOrdSend);
+        
 }
+function player2CoOrdSend(snapshot) {
+    var message = snapshot.val();
+  };
+function player1CoOrd() {
 
-function coChooser() {
-	var co = $(this).attr('class') + $(this).parent().attr('class');
+  ref1.on('child_added', function(snapshot) {
+    var message = snapshot.val();
+    console.log(message.co);
+    player1Choices.push(message.co);
+    console.log(player1Choices);
+    if (player1Choices.length === 15){
+     checkP1Match(player1Choices);
+
+    }
+});
+};
+function selectShips(){
+  if (maxShip === 15) {
+    alert("Out of Ships");
+    $(this).off(); 
+    return;}
+  
+    var co = $(this).attr('class') + $(this).parent().attr('class');
     $(this).addClass('ship');
-	console.log(co);
-	ref.push( {co: co });
-    
-}
-
-function getData(snapshot) {
-	var snapshot = snapshot.val();
-  console.log(snapshot);
-}
-
-
-var maxShip = 0;
-$('table').on('click', 'td', function(){
-  if (maxShip >=15) {
-  alert("Out of Ships");
-  return;
-  }
+    ref2.push( {co: co });
   
-  
-  var addShip = $(this).addClass('active');
     maxShip++
-    
-    $("td").addClass(addShip);
-    });
+    $(this).css("background-color", "blue");
+    $(this).off(); 
+};
 
+function checkP1Match(player1Choices) {
+    $('#guesses td').click( function() {
+      if (!myTurn) {return;}
+      var guessCoOrd = $(this).attr('class') + $(this).parent().attr('class');
+      if ( player1Choices.indexOf(guessCoOrd) !== -1 ) {
+        $(this).css('background-color', 'red');
+      } else {
+        $(this).css('background-color', 'green');
+      }
+      $(this).off();
+    
+      myTurn = false;
+      ref2G.push( {p2: guessCoOrd} );
+    })
+    };
